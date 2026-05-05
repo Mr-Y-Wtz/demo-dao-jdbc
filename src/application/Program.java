7d9f7065f043f3cd2925cc9be58f5088e3dfb7de
Mr-Y-Wtz <yehudi.witzel@gmail.com>
2026-05-05 12:28:03 -0300

    DepartmentDao and SellerDao interfaces

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,7 +4,6 @@ import model.entities.Department;
 import model.entities.Seller;
 
 import java.time.LocalDate;
-import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
